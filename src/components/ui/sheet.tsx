dfd177800eb3ab6c7b54ec197502572e53cf8fcc
James Allen <34049950+tjamesallen15@users.noreply.github.com>
2025-04-30 14:43:16 +0800

    Update sheet.tsx

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -74,7 +74,7 @@ function SheetContent({
         {children}
         {/* <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none"> */}
         <SheetPrimitive.Close className="absolute right-8 top-8 transition-opacity outline-none">
-          <IoMdClose className="text-3xl text-accent" />
+          <IoMdClose className="text-3xl text-primary hover:text-link-accent" />
           <span className="sr-only">Close</span>
         </SheetPrimitive.Close>
       </SheetPrimitive.Content>
